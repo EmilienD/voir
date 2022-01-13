6b54f3fa1f83c90cbda9fac0521ae1b853804c9d
Durieu Emilien <contact@emilienduri.eu>
2022-01-13 18:48:36 +0100

    feat: add download route

--- server/routes/root.js
+++ server/routes/root.js
@@ -1,29 +1,58 @@
 const fs = require('fs')
+const promisesFs = require('fs/promises')
 const path = require('path')
 const { v4: uuidV4 } = require('uuid')
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/stream', { websocket: true }, (connection) => {
+module.exports = async function (fastify) {
+  fastify.get('/upload-stream', { websocket: true }, (connection) => {
     let hasContent = false
-    const filePath = path.resolve(__dirname, `../../uploads/${uuidV4()}.webm`)
-    const fileStream = fs.createWriteStream(filePath, { encoding: 'binary' })
-    fileStream.on('error', (err) => {
+    const filePath = getFilePath(uuidV4())
+    const writeStream = fs.createWriteStream(filePath, { encoding: 'binary' })
+    writeStream.on('error', () => {
       //todo: handle errors
     })
-    fileStream.on('close', (ev) => {
+    writeStream.on('close', () => {
       if (!hasContent) {
         fs.rm(filePath, () => {})
       }
     })
-    connection.socket.on('message', (message, isBinary) => {
+    connection.socket.on('message', (message) => {
       hasContent = true
-      fileStream.write(message)
+      writeStream.write(message)
     })
-    connection.socket.on('close', (ev) => {
-      fileStream.close()
+    connection.socket.on('close', () => {
+      writeStream.close()
     })
-    connection.socket.on('error', (err) => {
+    connection.socket.on('error', () => {
       //todo: handle errors
     })
   })
+  fastify.get('/download-stream/:id', async (req, rep) => {
+    const filePath = getFilePath(req.params.id)
+    const fileStats = await promisesFs.stat(filePath)
+    const [queryStart, queryEnd] = (req.query.bytes || '')
+      .split('-')
+      .map((val) => Number.parseInt(val))
+    const start = queryStart || 0
+    const end =
+      queryEnd && queryEnd > 0 && queryEnd < fileStats.size
+        ? queryEnd
+        : fileStats.size - 1
+    const readStream = fs.createReadStream(filePath, {
+      start,
+      end,
+      autoClose: true,
+    })
+    rep
+      .code(206)
+      .headers({
+        'content-range': `bytes ${start}-${end}/`,
+        'accept-ranges': 'bytes',
+        'content-length': end - start + 1,
+        'content-type': 'video/webm',
+      })
+      .send(readStream)
+  })
 }
+
+const getFilePath = (id) => path.resolve(__dirname, `../../uploads/${id}.webm`)
