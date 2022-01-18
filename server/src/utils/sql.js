564d71f97678e58ec155fb9cba42720361f42ae7
Durieu Emilien <contact@emilienduri.eu>
2022-01-18 15:59:59 +0100

    feat: add HOFs to autocamelize db result
    Added packages for login and logout upcoming features.

--- server/src/utils/sql.js
+++ server/src/utils/sql.js
@@ -1,6 +1,13 @@
+const camelize = require('camelize')
+
 const sql = (string, ...args) => {
   const query = string.join('?')
   return [query, args]
 }
 
-module.exports = { sql }
+const select = (db) => (query) => (params) =>
+  camelize(db.prepare(query).get(params))
+const selectAll = (db) => (query) => (params) =>
+  camelize(db.prepare(query).all(params))
+
+module.exports = { sql, select, selectAll }
